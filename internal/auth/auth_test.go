e2c803b19d0ae889278d951cf086f38daba981c0
Meraiku <meraiku@gmail.com>
2024-08-18 21:26:40 +0300

    update test GetAPIKey

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -5,8 +5,8 @@ import (
 )
 
 func TestGetAPIKey(t *testing.T) {
-	t.Run("test getting API Key", func(t *testing.T) {
-		auth := map[string][]string{"Authorization": []string{"000", "oiwjd"}}
+	t.Run("test getting API Key with invalid API Key", func(t *testing.T) {
+		auth := map[string][]string{"Authorization": []string{"000"}}
 
 		_, err := GetAPIKey(auth)
 		if err == nil {
@@ -17,6 +17,34 @@ func TestGetAPIKey(t *testing.T) {
 
 		if err.Error() != want {
 			t.Fatalf("expected: %v, got: %v", want, err)
+		}
+
+	})
+
+	t.Run("test getting API Key without header", func(t *testing.T) {
+		auth := map[string][]string{"Auth": []string{}}
+
+		_, err := GetAPIKey(auth)
+		if err == nil {
+			t.Fatal("expected error")
+		}
+
+		want := "no authorization header included"
+
+		if err.Error() != want {
+			t.Fatalf("expected: %v, got: %v", want, err)
+		}
+
+	})
+
+	t.Run("test getting API Key with ApiKey", func(t *testing.T) {
+		auth := map[string][]string{"Authorization": []string{"ApiKey ayaya"}}
+
+		got, _ := GetAPIKey(auth)
+		want := "ayaya"
+
+		if got != want {
+			t.Fatalf("expected: %v, got: %v", want, got)
 		}
 
 	})
